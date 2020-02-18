6aabf5e0d6a9af53d81906b3107606bcda114216
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-02-17 22:35:31 -0500

    Remove users:login, users:logout, and users:signup paths

--- kaysworldAllAuth/users/urls.py
+++ kaysworldAllAuth/users/urls.py
@@ -6,8 +6,5 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('signup/', views.signup, name='signup'),
     path('<int:user_id>/home/', login_required(views.home), name='home'),
 ]
