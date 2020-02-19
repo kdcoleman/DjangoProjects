95c4b890fdba0d5863fd8d8e2abcc06ffcff82be
Kayla D. Coleman <kdcolem2@ncsu.edu>
2020-02-19 03:23:26 -0500

    Change forms to ModelForms; Add email format validation check

--- kaysworld/login/forms.py
+++ kaysworld/login/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.core import validators
+
 from .models import User
 # Create forms here.
 
-class LoginForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -21,18 +28,28 @@ class LoginForm(forms.Form):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        user_exists = User.objects.filter(email=email).exists()
-        if not user_exists:
-            raise forms.ValidationError("Account not found. Verify the email is correct.")
+        try:
+            validators.validate_email(email)
+            user_exists = User.objects.filter(email=email).exists()
+            if not user_exists:
+                self.add_error('email', forms.ValidationError("Account not found. Verify the email is correct."))
+        except forms.ValidationError:
+            raise forms.ValidationError("Enter a valid email address.")
 
         return email
 
 
-class SignupForm(forms.Form):
-    first_name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        }
+
     confirm_password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
 
     def clean(self):
@@ -47,9 +64,13 @@ class SignupForm(forms.Form):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        user_exists = User.objects.filter(email=email).exists()
-        if not user_exists:
-            return email
+        try:
+            validators.validate_email(email)
+            user_exists = User.objects.filter(email=email).exists()
+            if not user_exists:
+                return email
+        except forms.ValidationError:
+            raise forms.ValidationError("Enter a valid email address.")
         else:
             raise forms.ValidationError("Sorry, this email is already in use.")
 
